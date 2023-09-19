f5d37261c210bf0a0b5273d0c3983aadfba87232
gwendolin.hoffmann <gwendolin.hoffmann@7p-group.com>
2023-09-19 12:15:09 +0200

    Third part

---
+++ ors-engine/src/test/java/org/heigit/ors/routing/graphhopper/extensions/flagencoders/CargoBikeFlagEncoderTest.java
@@ -0,0 +1,38 @@
+package org.heigit.ors.routing.graphhopper.extensions.flagencoders;
+
+import com.graphhopper.reader.ReaderWay;
+import com.graphhopper.routing.util.EncodingManager;
+import org.heigit.ors.routing.graphhopper.extensions.ORSDefaultFlagEncoderFactory;
+import org.heigit.ors.routing.graphhopper.extensions.flagencoders.bike.CargoBikeFlagEncoder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CargoBikeFlagEncoderTest {
+
+    private final CargoBikeFlagEncoder flagEncoder;
+    private ReaderWay way;
+
+    public CargoBikeFlagEncoderTest() {
+        EncodingManager encodingManager = EncodingManager.create(new ORSDefaultFlagEncoderFactory(), FlagEncoderNames.BIKE_ORS);
+        flagEncoder = (CargoBikeFlagEncoder) encodingManager.getEncoder(FlagEncoderNames.BIKE_CARGO);
+    }
+
+    @BeforeEach
+    void initWay() {
+        way = new ReaderWay(1);
+    }
+
+    @Test
+    void acceptBridlewayOnlyWithBicycleTag() {
+        way.setTag("highway", "bridleway");
+        assertTrue(flagEncoder.getAccess(way).canSkip());
+
+        way.setTag("bicycle", "yes");
+        assertTrue(flagEncoder.getAccess(way).isWay());
+
+        way.setTag("bicycle", "no");
+        assertTrue(flagEncoder.getAccess(way).canSkip());
+    }
+}
